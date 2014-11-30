d0c30b7c750cf9cd8d5332ad749be72445b8d797
EarthScientist <lindgren.mike@gmail.com>
2014-11-29 23:44:44 -0900

    adds harvester script is a bare bones (non-working) form.

---
+++ nsidc_harvester.py
@@ -0,0 +1,10 @@
+import ftp
+
+ftp = ftplib.FTP( 'sidads.colorado.edu' )
+ftp.cwd( '/pub/DATASETS/nsidc0051_gsfc_nasateam_seaice/final-gsfc/north/monthly' )
+
+
+
+
+path = 'ftp://sidads.colorado.edu/pub/DATASETS/nsidc0051_gsfc_nasateam_seaice/final-gsfc/north/monthly'
+
